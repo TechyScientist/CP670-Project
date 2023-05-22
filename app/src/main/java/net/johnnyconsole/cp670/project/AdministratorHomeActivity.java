b8bebd4d8a03f05be5d263fba1b31ebb7ecc75ba
Johnny Console <johnny.console@algomau.ca>
2023-05-22 17:51:01 -0400

    Create layout and add actions for some functions of AdministratorHomeActivity

--- app/src/main/java/net/johnnyconsole/cp670/project/AdministratorHomeActivity.java
+++ app/src/main/java/net/johnnyconsole/cp670/project/AdministratorHomeActivity.java
@@ -1,28 +1,57 @@
 package net.johnnyconsole.cp670.project;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import net.johnnyconsole.cp670.project.databinding.ActivityStudentHomeBinding;
-
+import net.johnnyconsole.cp670.project.databinding.ActivityAdministratorHomeBinding;
 import java.util.Objects;
 
 public class AdministratorHomeActivity extends AppCompatActivity {
-    private ActivityStudentHomeBinding binding;
+    private ActivityAdministratorHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityStudentHomeBinding.inflate(getLayoutInflater());
+        binding = ActivityAdministratorHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
         Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.adminWelcome);
+        findViewById(R.id.btSchedule).setOnClickListener(view ->
+                startActivity(new Intent(this, CourseScheduleActivity.class))
+        );
+
+        /*
+         findViewById(R.id.btRegistration).setOnClickListener(view ->
+                startActivity(new Intent(this, RegistrationChangeActivity.class))
+        );
+
+         findViewById(R.id.btGrades).setOnClickListener(view ->
+                startActivity(new Intent(this, ViewGradesActivity.class))
+        );
+
+         findViewById(R.id.btProgress).setOnClickListener(view ->
+                startActivity(new Intent(this, DegreeProgressActivity.class))
+        );
+
+         findViewById(R.id.btChangepw).setOnClickListener(view ->
+                startActivity(new Intent(this, ChangePasswordActivity.class))
+        );
+        */
+        findViewById(R.id.btSignOut).setOnClickListener(view ->
+                new AlertDialog.Builder(this).setTitle(R.string.signout)
+                        .setMessage(R.string.confirmSignout)
+                        .setPositiveButton(R.string.exitYes, (dialog, id) -> finish())
+                        .setNegativeButton(R.string.exitNo, null)
+                        .create()
+                        .show()
+        );
     }
 
     @Override
@@ -44,5 +73,15 @@ public class AdministratorHomeActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this).setTitle(R.string.signout)
+                .setMessage(R.string.confirmSignout)
+                .setPositiveButton(R.string.exitYes, (dialog, id) -> finish())
+                .setNegativeButton(R.string.exitNo, null)
+                .create()
+                .show();
     }
 }
