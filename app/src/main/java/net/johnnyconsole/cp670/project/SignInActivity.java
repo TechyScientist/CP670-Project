d92cd045402bfae3d9774f74dfd253fee3669778
Johnny Console <johnny.console@algomau.ca>
2023-05-22 10:52:50 -0400

    Import layout and code for SignInActivity

--- app/src/main/java/net/johnnyconsole/cp670/project/SignInActivity.java
+++ app/src/main/java/net/johnnyconsole/cp670/project/SignInActivity.java
@@ -1,27 +1,26 @@
 package net.johnnyconsole.cp670.project;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import android.widget.EditText;
 
 import net.johnnyconsole.cp670.project.databinding.ActivitySignInBinding;
 
 import java.util.Objects;
+import static net.johnnyconsole.cp670.project.ApplicationSession.*;
 
 public class SignInActivity extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
+    private EditText etUsername, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +32,9 @@ public class SignInActivity extends AppCompatActivity {
         setSupportActionBar(binding.toolbar);
         Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.profile);
 
+        //Setting up activity view objects
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
 
     }
 
@@ -49,5 +51,74 @@ public class SignInActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /*
+       onSignInClicked(View v)
+       OnClickListener for the sign in button
+       Retreives the user's information from the database and if
+       the username is found and the password matches, show the right
+       activity based on their user account type
+    */
+    public void onSignInClicked(View v) {
+        String user = etUsername.getText().toString().toLowerCase(),
+                password = etPassword.getText().toString();
+
+        //If the username or password are empty, show an error dialog
+        if(user.isEmpty() || password.isEmpty()) {
+            showErrorDialog();
+        }
+        else {
+            //Check the database for the entered username
+            Cursor c = database.rawQuery("SELECT * FROM users WHERE username=?;", new String[] {user});
+
+            //If there is a row, check the password.
+            if(c.moveToFirst()) {
+                //If the password matches, set up the session variables in ApplicationSession
+                if(password.equals(c.getString(4))) {
+                    username = user;
+                    first = c.getString(2);
+                    last = c.getString(1);
+                    userType = c.getString(3);
+                    c.close();
+
+                    //Clear the username and password fields
+                    etUsername.getText().clear();
+                    etPassword.getText().clear();
+
+                   /* //If the user is a student, show the student home page
+                    if(userType.equals("student")) {
+                        startActivity(new Intent(this, StudentHomeActivity.class));
+                    }
+                    //Otherwise, we know that the user must be an administrator, so show the
+                    //administrator home page. Future iterations may distinguish between faculty
+                    //and administrative users for purposes of grade entering, time-dependent.
+                    else {
+                        startActivity(new Intent(this, AdministratorHomeActivity.class));
+                    }*/
+                }
+                //Show an error dialog if the user's password is incorrect
+                else {
+                    showErrorDialog();
+                }
+            }
+            //Show an error dialog if the user isn't in the database
+            else {
+                showErrorDialog();
+            }
+        }
+    }
+
+    /*
+        showErrorDialog()
+        Creates and shows an error dialog.
+        Used if there is an error during the sign in checking.
+     */
+    private void showErrorDialog() {
+        new AlertDialog.Builder(this).setTitle(getString(R.string.signinErrorTitle))
+                .setMessage(getString(R.string.signinErrorMessage))
+                .setPositiveButton(getString(R.string.dismiss), null)
+                .create()
+                .show();
     }
 }
